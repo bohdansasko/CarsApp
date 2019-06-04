49e3408f10d22a199182cf2bb4c530607cfc7fa3
bohdans <0000xffff@gmail.com>
2019-06-04 18:25:56 +0300

    initial commit

---
+++ CarsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarsApp
+//
+//  Created by Bogdan Sasko on 6/4/19.
+//  Copyright © 2019 Vinso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
